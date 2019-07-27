743336f5b60214b5b5bdfd862f426f18c4dbbaa0
Nikhil Kataria <nikhilkataria19@gmail.com>
2019-07-28 02:36:29 +0530

    adding test cases for premium customers

--- src/test/java/com/customer/service/CustomerServiceSpringTest.java
+++ src/test/java/com/customer/service/CustomerServiceSpringTest.java
@@ -25,7 +25,7 @@ public class CustomerServiceSpringTest {
 		Billing billing = BillingTestData.getBillingWithPremiumCustomerHavingAmountGreaterThan16000();
 		BigDecimal discount = customerService.calculateBillAmount(billing);
 		Assert.assertNotNull(discount);
-		System.out.println(discount);
+		Assert.assertEquals(BigDecimal.valueOf(3300.0), discount);
 	}
 
 	@Test
@@ -33,7 +33,7 @@ public class CustomerServiceSpringTest {
 		Billing billing = BillingTestData.getBillingWithPremiumCustomerHavingAmountEquals16000();
 		BigDecimal discount = customerService.calculateBillAmount(billing);
 		Assert.assertNotNull(discount);
-		Assert.assertEquals(BigDecimal.valueOf(3000), discount);
+		Assert.assertEquals(BigDecimal.valueOf(3000.0), discount);
 	}
 
 	@Test
@@ -41,6 +41,8 @@ public class CustomerServiceSpringTest {
 		Billing billing = BillingTestData.getBillingWithPremiumCustomerHavingAmountLessThan16000AndGreaterThan12000();
 		BigDecimal discount = customerService.calculateBillAmount(billing);
 		Assert.assertNotNull(discount);
+		Assert.assertEquals(BigDecimal.valueOf(2700.0), discount);
+
 	}
 
 	@Test
@@ -48,39 +50,52 @@ public class CustomerServiceSpringTest {
 		Billing billing = BillingTestData.getBillingWithPremiumCustomerHavingAMountEquals12000();
 		BigDecimal discount = customerService.calculateBillAmount(billing);
 		Assert.assertNotNull(discount);
-		System.out.println(discount);
+		Assert.assertEquals(BigDecimal.valueOf(1800.0), discount);
+
 	}
-	
+
 	@Test
 	public void test_discountonpurchasingamount_whenPurchasingLessThan12000GreaterThan8000_onPremiumCustomer() {
 		Billing billing = BillingTestData.getBillingWithPremiumCustomerHavingAmountsLessThan12000ANdGreaterThan8000();
 		BigDecimal discount = customerService.calculateBillAmount(billing);
 		Assert.assertNotNull(discount);
-		System.out.println(discount);
+		Assert.assertEquals(BigDecimal.valueOf(1200.0), discount);
+
 	}
-	
+
 	@Test
 	public void test_discountonpurchasingamount_whenPurchasingEquals8000_onPremiumCustomer() {
-		Billing billing = BillingTestData.getBillingWithPremiumCustomerHavingAmountsEquals8000()
+		Billing billing = BillingTestData.getBillingWithPremiumCustomerHavingAmountsEquals8000();
 		BigDecimal discount = customerService.calculateBillAmount(billing);
 		Assert.assertNotNull(discount);
-		System.out.println(discount);
+		Assert.assertEquals(BigDecimal.valueOf(1000.0), discount);
+
 	}
-	
+
 	@Test
-	public void test_discountonpurchasingamount_whenPurchasingLessThan8000__onPremiumCustomer() {
-		Billing billing = BillingTestData.getBillingWithPremiumCustomerHavingAMountEquals12000();
+	public void test_discountonpurchasingamount_whenPurchasingLessThan8000GreaterThan4000__onPremiumCustomer() {
+		Billing billing = BillingTestData.getBillingWithPremiumCustomerHavingAmountsLessThan8000AndGreaterThan4000();
 		BigDecimal discount = customerService.calculateBillAmount(billing);
 		Assert.assertNotNull(discount);
-		System.out.println(discount);
+		Assert.assertEquals(BigDecimal.valueOf(850.0), discount);
+
 	}
-	
-//	@Test
-//	public void test_discountonpurchasingamount_whenPurchasingEquals12000_onPremiumCustomer() {
-//		Billing billing = BillingTestData.getBillingWithPremiumCustomerHavingAMountEquals12000();
-//		BigDecimal discount = customerService.calculateBillAmount(billing);
-//		Assert.assertNotNull(discount);
-//		System.out.println(discount);
-//	}
+
+	@Test
+	public void test_discountonpurchasingamount_whenPurchasingEquals4000_onPremiumCustomer() {
+		Billing billing = BillingTestData.getBillingWithPremiumCustomerHavingAmountsEquals4000();
+		BigDecimal discount = customerService.calculateBillAmount(billing);
+		Assert.assertNotNull(discount);
+		Assert.assertEquals(BigDecimal.valueOf(400.0), discount);
+
+	}
+
+	@Test
+	public void test_discountonpurchasingamount_whenPurchasingLessThan4000GreaterThan0_onPremiumCustomer() {
+		Billing billing = BillingTestData.getBillingWithPremiumCustomerHavingAmountsLessThan4000();
+		BigDecimal discount = customerService.calculateBillAmount(billing);
+		Assert.assertNotNull(discount);
+		Assert.assertEquals(BigDecimal.valueOf(300.0), discount);
+	}
 
 }
